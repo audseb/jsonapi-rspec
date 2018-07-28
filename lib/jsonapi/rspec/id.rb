ff956632ce1b6c752a92f8624a9d4db534d90172
Tony D'Addeo <tony@askhr.com>
2018-07-28 16:53:37 -0500

    Comparse id as string

--- lib/jsonapi/rspec/id.rb
+++ lib/jsonapi/rspec/id.rb
@@ -2,7 +2,7 @@ module JSONAPI
   module RSpec
     module Id
       ::RSpec::Matchers.define :have_id do |expected|
-        match { |actual| actual['id'] == expected }
+        match { |actual| actual['id'] == expected.to_s }
       end
     end
   end
